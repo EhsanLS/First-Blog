e74aa918e20560c70c99f39bb58a639c752ce300
EhsanLS <ehsanazadeh1@gmail.com>
2025-02-08 16:38:20 +0330

    move setting to main urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,3 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('post/<slug:slug>/', views.article_detail, name='article_detail')
 ]
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
